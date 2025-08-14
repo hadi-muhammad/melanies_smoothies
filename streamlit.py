be22674cc46a639fe81565009aecaa0c6635122d
hadi-muhammad <68080827+hadi-muhammad@users.noreply.github.com>
2025-08-14 10:49:01 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -26,8 +26,14 @@ st.write('The name on your Smoothie will be:', name_on_order)
 # session = get_active_session()
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'),col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data = my_dataframe, use_container_widthTrue)
+# st.stop()
+
+pd_df = my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -43,8 +49,11 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        # st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_reponse = requests.get('https://my.smoothiefroot.com/api/fruit/' + fruit_chosen)
+        smoothiefroot_reponse = requests.get('https://my.smoothiefroot.com/api/fruit/' + search_on)
         sf_df = st.dataframe(data=smoothiefroot_reponse.json(),use_container_width=True)
 
     # st.write(ingredients_string)
